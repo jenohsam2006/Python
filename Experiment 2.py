44247f7e9819f48f547f30fde4c14a20600e769a
Jenoh Sam J B <jenohsam@karunya.edu.in>
2025-01-31 11:35:38 +0530

    Experiment 2.py

--- Experiment 2.py
+++ Experiment 2.py
@@ -1,4 +1,7 @@
-1
+#Jenoh Sam J B
+#URK24CS1154
+#Second smallest and Second largest number in a list
+print("URK24CS1154")
 l = [12, 45, 2, 9, 3,5,45]
 n=set(l)
 l=list(n)
@@ -6,15 +9,21 @@ print(l)
 print("Second smallest:", l[1])
 print("Second largest:", l[-2]) 
 
-2
+#Jenoh Sam J B
+#URK24CS1154
+#Frequency of the elements in a list
+print("URK24CS1154")
 e = ['apple', 'banana', 'apple', 'orange','banana','apple']
 for i in set(e):
     c = e.count(i)
-    print(f'{i}: {c}')
-3
+    print(i," : ",c)
 
+#Jenoh Sam J B
+#URK24CS1154
+#Provided person names and mobile numbers as tuples in a list and search for mobile numbers based on a given person's name.
+print("URK24CS1154")
 c = []
-for i in range(2):
+for i in range(3):
     n = input("Enter name: ")
     m = input("Enter mobile number: ")
     c.append((n,m))
@@ -22,12 +31,15 @@ print(c)
 sn = input("Enter the person name to search: ")
 for co in c:
     if co[0] == sn:
-        print(f"Mobile Number is {co[1]}")
+        print("Mobile Number is : ",co[1])
         break
 else:
-    print("No contact found for", {sn})
-4
+    print("No contact found for ", sn)
 
+#Jenoh Sam J B
+#URK24CS1154
+#Demonstrate all the methods in set
+print("URK24CS1154")
 a = {1, 2, 3, 4, 5}
 b = {4, 5, 6, 7, 8}
 print("Union :", a | b)
