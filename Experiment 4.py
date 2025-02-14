c7a260372735411fc04dfdc083c0794d88e1a467
Jenoh Sam J B <jenohsam@karunya.edu.in>
2025-02-14 12:15:27 +0530

    Create Experiment 4.py

---
+++ Experiment 4.py
@@ -0,0 +1,43 @@
+def perfect (n):
+    s = 0
+    for i in range(1, n):
+        if n % i == 0:
+            s += i
+    if s == n:
+        return True
+    else:
+        return False
+nu = int(input("Enter a number: "))
+if perfect(nu):
+    print(nu,"is a perfect number.")
+else:
+    print(nu,"is not a perfect number.")
+
+
+
+def happy (n):
+    s = set()
+    while n != 1 and n not in s:
+        s.add(n)
+        n = sum(int(d) ** 2 for d in str(n))
+    return n == 1
+n = int(input("Enter a number: "))
+if happy(n):
+    print(n," is a Happy Number.")
+else:
+    (n," is not a Happy Number.")
+
+
+def fun1(*args):
+    l = args[-1]
+    d = []
+    for i in range(1, l + 1):
+        if l % i == 0:
+            d.append(i)
+    print(l,":", end=" ")
+    for ds in d:
+        print(ds, end=" ")
+    print()
+fun1(6, 7, 8)
+fun1(1, 2, 5, 7, 9)
+fun1(1, 3, 5, 7, 9, 12)
