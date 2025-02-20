d20ee0f0b2baf6e243869ee5bf9eff75e575691f
Jenoh Sam J B <jenohsam@karunya.edu.in>
2025-02-20 14:52:17 +0530

    Create Experiment 5.py

---
+++ Experiment 5.py
@@ -0,0 +1,20 @@
+#Jenoh Sam J B
+#URK24CS1154
+#A number is perfect Number or not using a function.
+print("URK24CS1154")
+inventory = { 
+    'gold': 500,
+    'pouch': ['flint', 'twine', 'gemstone'],
+    'backpack': ['xylophone', 'dagger', 'bedroll', 'bread loaf']
+}
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] += 50
+print(inventory)
+
+
+#Jenoh Sam J B
+#URK24CS1154
+#A number is perfect Number or not using a function.
+print("URK24CS1154")
